380a26ba315f9fd578a4b6c814770735daa219e7
Ian Chen <ianchen06@gmail.com>
2024-03-06 11:42:12 -0800

    wip

--- ⛷️_Home.py
+++ ⛷️_Home.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Winter Gear Guru | Eco-Friendly Gear Analysis",
     page_icon="⛷️",
-    layout="centered",
+    layout="wide",
     initial_sidebar_state="auto",
     menu_items=None,
 )
@@ -15,4 +15,21 @@ st.image("images/home.jpg", use_column_width=True)
 
 st.sidebar.success("Select a page above.")
 
-st.button("Get Eco friendly gear advice", type="primary")
+def nav_to(url):
+    nav_script = """
+        <meta http-equiv="refresh" content="0; url='%s'">
+    """ % (url)
+    st.write(nav_script, unsafe_allow_html=True)
+
+st.header("Main Features")
+
+cols = st.columns(3)
+with cols[0]:
+    st.image("images/gear.webp", use_column_width=True)
+    st.button("Eco friendly gear analysis", type="primary", on_click=lambda: nav_to("/Gear_Analyzer"))
+with cols[1]:
+    st.image("images/chat.webp", use_column_width=True)
+    st.button("Chat with Gear Guru", type="primary", on_click=lambda: nav_to("/Chat"))
+with cols[2]:
+    st.image("images/about.webp", use_column_width=True)
+    st.button("Learn how to protect our winters", type="primary", on_click=lambda: nav_to("/About"))
